52a695feae1505c0f8ed978e917ee609bf595669
Abdullah-hub498 <abidabdullah498@gmail.com>
2022-02-20 11:31:53 +0500

    testing app

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+  Button,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("click");
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>Hello React Native World!</Text>
+      <TouchableOpacity onPress={() => console.log("image click")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableOpacity>
+      <Button title="Click Me" onPress={() => console.log("Button press")} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
