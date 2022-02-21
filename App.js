10f9f6e27b123a449ded0e338a281d374ff96b55
Abdullah-hub498 <abidabdullah498@gmail.com>
2022-02-21 22:56:58 +0500

    design welcome screen

--- App.js
+++ App.js
@@ -1,38 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  SafeAreaView,
-  TouchableOpacity,
-  Button,
-} from "react-native";
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  const handlePress = () => console.log("click");
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text onPress={handlePress}>Hello React Native World!</Text>
-      <TouchableOpacity onPress={() => console.log("image click")}>
-        <Image
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableOpacity>
-      <Button title="Click Me" onPress={() => console.log("Button press")} />
-    </SafeAreaView>
-  );
+  console.log("ok");
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ffff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
